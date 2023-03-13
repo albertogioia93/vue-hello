eb84270ac683aa5837b509951e8ee82797268196
albertogioia93 <albertogioia@hotmail.it>
2023-03-13 16:03:37 +0100

    riflessioni su riga 9 di main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const app = Vue.createApp({
         return {
             messaggio: 'Concerto di Mezzosangue al Teatro della Concordia di Venaria Reale (Torino) il 28 aprile 2023',
             imageUrl: 'img/mezzosangueimg.jpg',
+            // non ho usato un numero ma una stringa contenente un numero (500) e una parola (px). sul browser funziona, l'img viene ridimensionata
             imageWidth: '500px',
             imageHeight: 'auto'  
         }
